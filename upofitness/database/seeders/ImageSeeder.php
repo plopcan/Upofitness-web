ea13442db12e23fa8814b4ff02244cc0521cdd3a
alexmiguelgom <amiggom@alu.upo.es>
2025-04-23 10:32:28 +0200

    #22 Seeder de Image

--- upofitness/database/seeders/ImageSeeder.php
+++ upofitness/database/seeders/ImageSeeder.php
@@ -1,18 +1,24 @@
 <?php
 
 namespace Database\Seeders;
-use App\Models\Image;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use App\Models\Image;
 
 class ImageSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        
+        Image::create([
+            'product_id' => 1,
+            'url' => 'products/DLQviXpZrBZ6FrUslczj0WCyFByW7Gnt856hwCcG.jpg',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
     }
 }
