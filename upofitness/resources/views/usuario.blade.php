a691f93261137e08cc6d290e82ad49fcbc0ec3de
Gonzalo <146015965+html5Enjoyer@users.noreply.github.com>
2025-04-26 19:15:04 +0200

    Eliminacion de autenticacion por variable de sesion #25

--- upofitness/resources/views/usuario.blade.php
+++ upofitness/resources/views/usuario.blade.php
@@ -8,13 +8,8 @@
 <body>
     <nav>
         <a href="{{ route('productos.index') }}">Catálogo</a>
-        @if (session('usuario_id'))
-            <a href="{{ route('cart.showByUserId', ['id' => session('usuario_id')]) }}">Carrito</a>
-            <a href="{{ route('wishlist.showByUserId', ['id' => session('usuario_id')]) }}">Lista de Deseos</a>
-        @else
-            <a href="#" onclick="alert('Debes iniciar sesión para acceder al carrito.')">Carrito</a>
-            <a href="#" onclick="alert('Debes iniciar sesión para acceder a la lista de deseos.')">Lista de Deseos</a>
-        @endif
+        <a href="{{ route('cart.showByUserId', ['id' => session('usuario_id')]) }}">Carrito</a>
+        <a href="{{ route('wishlist.showByUserId', ['id' => session('usuario_id')]) }}">Lista de Deseos</a>
         <a href="#profile">Perfil</a>
     </nav>
 
