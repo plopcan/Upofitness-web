df696c26686102598fe1b49439e34539108936a7
alexmiguelgom <amiggom@alu.upo.es>
2025-04-26 17:48:57 +0200

    Seeder de wishlist_product #22

--- upofitness/database/seeders/WishlistSeeder.php
+++ upofitness/database/seeders/WishlistSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Wishlist;
 use App\Models\Usuario;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class WishlistSeeder extends Seeder
 {
@@ -29,5 +30,27 @@ class WishlistSeeder extends Seeder
 
 
         }
+
+        $wishlists = Wishlist::all();
+        $products = \App\Models\Product::all();
+
+        if ($products->isEmpty()) {
+            $this->command->error('No hay productos para añadir a las listas de deseos. Asegúrate de ejecutar ProductSeeder primero.');
+            return;
+        }
+
+        foreach ($wishlists as $wishlist) {
+            $randomProducts = $products->random(rand(1, 5));
+            
+            foreach ($randomProducts as $product) {
+                DB::table('wishlist_product')->insert([
+                    'wishlist_id' => $wishlist->id,
+                    'product_id' => $product->id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
+
     }
 }
